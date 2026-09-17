66c67135aa0dffb8dea8d1444d546180c556ce80
jelalqaiumi <qaiumi@hotmail.com>
2026-09-17 12:47:12 +0200

    Fel i koden som är rättat.

--- Mitt-projekt-backend/Data/MovieData.cs
+++ Mitt-projekt-backend/Data/MovieData.cs
@@ -15,7 +15,7 @@ namespace Mitt_projekt_backend.Data
 			new Movie { Id = 4, Title = "Inception", Genre = "Sci-Fi", Year = 2010, Rating = 8.8,
 				Description = "En tjuv stjäl hemligheter genom att gå in i människors drömmar.", ImageUrl = "" },
 			new Movie { Id = 5, Title = "Pulp Fiction", Genre = "Kriminal", Year = 1994, Rating = 8.9,
-			    Description = "En flicka hamnar i en magisk värld full av andar.", ImageUrl = "" },
+			    Description = "Flera kriminella historier i Los Angeles flätas samman.", ImageUrl = "" },
 			new Movie { Id = 6, Title = "Forrest Gump", Genre = "Drama", Year = 1994, Rating = 8.8,
 				Description = "En godhjärtad man är med om flera stora händelser i USA:s historia.", ImageUrl = "" },
 			new Movie { Id = 7, Title = "The Matrix", Genre = "Sci-Fi", Year = 1999, Rating = 8.7,
@@ -25,8 +25,8 @@ namespace Mitt_projekt_backend.Data
 			new Movie { Id = 9, Title = "Gladiator", Genre = "Action", Year = 2000, Rating = 8.5,
 				Description = "En romersk general blir slav och kämpar som gladiator för hämnd.", ImageUrl = "" },
 			new Movie { Id = 10, Title = "Spirited Away", Genre = "Animerad", Year = 2001, Rating = 8.6,
-	}           Description = "En flicka hamnar i en magisk värld full av andar.", ImageUrl = "" },
-}           new Movie { Id = 11, Title = "The Lion King", Genre = "Animerad", Year = 1994, Rating = 8.5,
+	            Description = "En flicka hamnar i en magisk värld full av andar.", ImageUrl = "" },
+            new Movie { Id = 11, Title = "The Lion King", Genre = "Animerad", Year = 1994, Rating = 8.5,
                 Description = "Ett ungt lejon måste ta sin plats som kung.", ImageUrl = "" },
             new Movie { Id = 12, Title = "Parasite", Genre = "Thriller", Year = 2019, Rating = 8.5,
                 Description = "En fattig familj tar sig in i en rik familjs liv.", ImageUrl = "" },
@@ -46,3 +46,9 @@ namespace Mitt_projekt_backend.Data
                 Description = "Ett besök hos flickvännens familj blir allt mer obehagligt.", ImageUrl = "" },
             new Movie { Id = 20, Title = "The Conjuring", Genre = "Skräck", Year = 2013, Rating = 7.5,
                 Description = "Två paranormala utredare hjälper en familj i ett hemsökt hus.", ImageUrl = "" }
+			 };
+    }
+}
+
+			  
+    
