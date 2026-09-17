eb6ef98556526804c0e962e983df68928d82e955
jelalqaiumi <qaiumi@hotmail.com>
2026-09-17 09:17:10 +0200

    Lagt till datatyp i klassen movie.cs

---
+++ Mitt-projekt-backend/Models/Movie.cs
@@ -0,0 +1,13 @@
+namespace Mitt_projekt_backend.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Genre { get; set; } = "";
+        public int Year { get; set; }
+        public double Rating { get; set; }
+        public string ImageUrl { get; set; } = "";
+    }
+}
